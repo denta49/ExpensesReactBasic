d99191d98a7c71eb46b045b692255298e30b821a
przemekWojenka <pdentaw@gmail.com>
2022-06-22 10:28:47 +0200

    changed dir structure

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,18 +1,27 @@
 import "./ExpenseItem.css"
 import ExpenseDate from "./ExpenseDate";
-import Card from "./Card";
+import Card from "../UI/Card";
 import './ExpenseItem.css';
+import React, { useState } from "react";
 
 const ExpenseItem = (props) => {
+
+    const [title, setTitle] = useState(props.title);
+
+    const clickHandler = () => {
+        setTitle(2);
+
+    }
 
     return (
 
         <Card className='expense-item'>
             <ExpenseDate date={props.date} />
             <div className='expense-item__description'>
-                <h2>{props.title}</h2>
+                <h2>{title}</h2>
                 <div className='expense-item__price'>${props.amount}</div>
             </div>
+            <button onClick={clickHandler}>Change Title</button>
         </Card>
     )
 }
