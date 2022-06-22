8b4f0a20f6e47d8753c4ac01bf24f553b142c7df
przemekWojenka <pdentaw@gmail.com>
2022-06-22 10:49:54 +0200

    add new extense form

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,10 +1,12 @@
 import React from "react";
 import './NewExpense.css';
+import ExpenseForm from "./ExpenseForm.js";
 
 const NewExpense = () => {
 
     return (
-        <div>
+        <div className='new-expense'>
+            <ExpenseForm />
         </div>
     )
 
